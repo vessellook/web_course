e6f30e263d97b775fc9df81e667fbaf7bce5a97f
vessellook <vessellook@gmail.com>
2021-12-26 09:22:11 +0300

    Add changes in database structure

--- etc/recreate-database.sql
+++ etc/recreate-database.sql
@@ -4,79 +4,47 @@ CREATE TABLE product
     id    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     uid   VARCHAR(255) NOT NULL UNIQUE KEY,
     name  VARCHAR(255) NOT NULL,
-    price INT UNSIGNED NOT NULL COMMENT 'в копейках',
-    count INT UNSIGNED NOT NULL
+    price INT UNSIGNED NOT NULL COMMENT 'в тысячах рублей'
 );
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user
+DROP TABLE IF EXISTS customer;
+CREATE TABLE customer
 (
-    id            INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    role          ENUM('customer', 'operator', 'director'),
-    login         VARCHAR(255) DEFAULT NULL UNIQUE KEY,
-    email         VARCHAR(255) DEFAULT NULL UNIQUE KEY,
-    phone_number  VARCHAR(255) DEFAULT NULL,
-    password_hash VARCHAR(255) DEFAULT NULL,
-    name          VARCHAR(255) NOT NULL
+    id           INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(255) NOT NULL UNIQUE KEY,
+    address      VARCHAR(2000) DEFAULT NULL,
+    phone_number VARCHAR(255)  DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order`
 (
-    id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNSIGNED REFERENCES user (id),
-    address TEXT,
-    status  ENUM('created',
-        'fixed',
-        'transportation_offered',
-        'transportation_approved',
-        'agreement_loaded',
-        'agreement_rejected',
-        'agreement_confirmed',
-        'paid',
-        'completed'),
-    price   INT UNSIGNED COMMENT 'в копейках'
-);
-
-DROP TABLE IF EXISTS agreement_template;
-CREATE TABLE agreement_template
-(
-    id       INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    order_id INT UNSIGNED REFERENCES `order`(id),
-    status   ENUM('enabled', 'disabled'),
-    hash     VARCHAR(255),
-    path     VARCHAR(4096)
-);
-
-DROP TABLE IF EXISTS agreement;
-CREATE TABLE agreement
-(
-    id                    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    order_id              INT UNSIGNED REFERENCES `order`(id),
-    agreement_template_id INT UNSIGNED REFERENCES agreement_template(id),
-    status                ENUM( 'loaded',
-        'rejected',
-        'removed',
-        'confirmed'),
-    hash                  VARCHAR(255),
-    path                  VARCHAR(4096)
-);
-
-DROP TABLE IF EXISTS order_product;
-CREATE TABLE order_product
-(
-    id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    product_id INT UNSIGNED,
-    order_id   INT UNSIGNED,
-    count      INT UNSIGNED
+    id             INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    customer_id    INT UNSIGNED  NOT NULL REFERENCES customer (id),
+    product_id     INT UNSIGNED  NOT NULL REFERENCES product (id),
+    address        VARCHAR(2000) NOT NULL,
+    date           DATE         DEFAULT NULL,
+    agreement_code VARCHAR(255) DEFAULT NULL,
+    agreement_url  VARCHAR(255) DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS transportation;
 CREATE TABLE transportation
 (
+    id           INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    order_id     INT UNSIGNED                 NOT NULL REFERENCES `order` (id),
+    planned_date DATE                         NOT NULL,
+    real_date    DATE DEFAULT NULL,
+    number       INT UNSIGNED                 NOT NULL,
+    status       ENUM ('planned', 'finished') NOT NULL
+);
+
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user
+(
     id       INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    order_id INT UNSIGNED REFERENCES `order`(id),
-    date     DATE,
-    address  TEXT,
-    status   VARCHAR(255)
+    role     ENUM ('operator', 'director') NOT NULL,
+    login    VARCHAR(255) DEFAULT NULL UNIQUE KEY,
+    password VARCHAR(255) DEFAULT NULL
 );
